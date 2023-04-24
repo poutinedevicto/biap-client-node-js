a692c0af2d046d8755b0db3ef84f43ac1eff876b
Abhijeet Singh Rathor <abhijeet@Wil-Admins-MacBook-Air-3.local>
2023-04-24 20:25:57 +0530

    fix: location

--- src/order/select/bppSelect.service.js
+++ src/order/select/bppSelect.service.js
@@ -1,50 +1,49 @@
 import { protocolSelect } from "../../utils/protocolApis/index.js";
 
 class BppSelectService {
+  /**
+   * bpp select order
+   * @param {Object} context
+   * @param {Object} order
+   * @returns
+   */
+  async select(context, order = {}) {
+    try {
+      const { cart = {}, fulfillments = [] } = order || {};
 
-    /**
-    * bpp select order
-    * @param {Object} context 
-    * @param {Object} order 
-    * @returns 
-    */
-    async select(context, order = {}) {
-        try {
-            const { cart = {}, fulfillments = [] } = order || {};
+      const provider = cart?.items?.[0]?.provider || {};
 
-            const provider = cart?.items?.[0]?.provider || {};
+      const selectRequest = {
+        context: context,
+        message: {
+          order: {
+            items:
+              cart.items.map((cartItem) => {
+                return {
+                  id: cartItem?.id?.toString(),
+                  quantity: cartItem?.quantity,
+                  location_id: "SIVA-ONDC-STORE-1-LOC-1",
+                };
+              }) || [],
+            provider: {
+              id: provider?.id,
+              locations: provider.locations.map((location) => {
+                return { id: location };
+              }),
+            },
+            fulfillments:
+              fulfillments && fulfillments.length ? [...fulfillments] : [],
+          },
+        },
+      };
 
-            const selectRequest = {
-                context: context,
-                message: {
-                    order: {
-                        items: cart.items.map(cartItem => {
-                            return {
-                                id: cartItem?.id?.toString(),
-                                quantity: cartItem?.quantity
-                            }
-                        }) || [],
-                        provider: {
-                            id: provider?.id,
-                            locations: provider.locations.map(location => {
-                                return { id: location };
-                            })
-                        },
-                        fulfillments: fulfillments && fulfillments.length ? 
-                            [...fulfillments] : 
-                            []
-                    }
-                }
-            };
+      const response = await protocolSelect(selectRequest);
 
-            const response = await protocolSelect(selectRequest);
-
-            return { context: context, message: response.message };
-        }
-        catch (err) {
-            throw err;
-        }
+      return { context: context, message: response.message };
+    } catch (err) {
+      throw err;
     }
+  }
 }
 
 export default BppSelectService;
